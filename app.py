4d5f98f5018bc35ebc43848d58db470b9173d634
Antonino545 <80984937+Antonino545@users.noreply.github.com>
2023-10-11 18:05:31 +0200

    First commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify
+import gttorari
+
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run()
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+@app.route('/fermata/<int:fermata>', methods=['GET'])
+def get_fermata(fermata):
+    return gttorari.printout(gttorari.gttorari_stop(fermata))
+
+
+@app.route('/fermata/<int:fermata>/<int:line>', methods=['GET'])
+def get_fermata_line(fermata, line):
+    return gttorari.printout(gttorari.gttorari_stop_line(fermata, line))
+
+
+
+@app.route('/flutter/fermata/<int:fermata>/json', methods=['GET'])
+def get_fermata_json(fermata):
+    return jsonify(gttorari.gttorari_stop(fermata))
+
+
+@app.route('/flutter/fermata/<int:fermata>/<int:line>/json', methods=['GET'])
+def get_fermata_line_json(fermata, line):
+    return jsonify(gttorari.gttorari_stop_line(fermata, line))
+
+
---
+++ gttorari.py
@@ -0,0 +1,69 @@
+import datetime
+import bs4
+import requests
+import json
+import pytz
+
+
+# classe fermata
+
+
+def jsonout(data):
+    if data is None:
+        print("non ci sono dati di passaggio per questa fermata o non è stata inserita una fermata valida")
+    else:
+        for bus_line, direction, pas in data:
+            print(json.dumps({"linea": bus_line, "direzione": direction, "passaggi": pas}, indent=4))
+
+
+def printout(data):
+    var = ""
+    for bus_line, direction, pas, nextpass in data:
+        var += "Linea: " + bus_line + "<br>"
+        var += "Direzione: " + direction + "<br>"
+        var += "Passaggi: " + pas + "<br>"
+        if nextpass <= 1:
+            var += "Prossimo passaggio tra meno di un minuto" + "<br>"
+        else:
+            var += "Prossimo passaggio: " + str(nextpass) + " minuti" + "<br>"
+    return var
+
+
+def gttorari_url(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = bs4.BeautifulSoup(response.text, 'html.parser')
+    data = []
+    table = soup.find('tbody')
+    if table:
+        rows = table.find_all('tr')
+        for row in rows:
+            cols = row.find_all('td')
+            bus_line = cols[0].find('a').text.strip()
+            direction = cols[1].text.strip()
+            pas = cols[2].text.replace(
+                "Al momento non ci sono previsioni in tempo reale, clicca qui per visualizzare i passaggi programmati.",
+                "").strip()
+            time_str = pas.split('*')[0]
+            hours, minutes = map(int, time_str.split(':'))
+            rome_timezone = pytz.timezone('Europe/Rome')  # Set the time zone to Rome
+            now = datetime.datetime.now(rome_timezone)
+            nextpass = (hours * 60 + minutes) - (now.hour * 60 + now.minute)
+            data.append((bus_line, direction, pas, nextpass))
+
+    return data
+
+
+def gttorari_stop(stop):
+    if isinstance(stop, int):
+        pre = "https://www.gtt.to.it/cms/percorari/arrivi?palina="
+        post = "&bacino="
+        url = pre + str(stop) + post
+        return gttorari_url(url)
+    else:
+        return print("La fermata inserita non è valida")
+
+
+def gttorari_stop_line(stop, line):
+    data = gttorari_stop(stop)
+    return [(bus, temp, pas, nextpass) for bus, temp, pas, nextpass in data if bus == str(line)]
