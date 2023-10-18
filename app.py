afd8146838f9065fb657bea3d1ceee43a8b7a322
antonino <antonino.incorvaia@edu.unito.it>
2023-10-18 12:00:36 +0200

    change in error output

--- app.py
+++ app.py
@@ -11,21 +11,24 @@ if __name__ == '__main__':
 def hello():
     return render_template('index.html')
 
+
 @app.route('/orari_fermata')
 def orari_fermata():
     return "Orari fermata"
 
+
 @app.route('/chi_sono')
 def chi_sono():
     return render_template('whoare.html')
+
+
 @app.route('/fermata/<int:fermata>', methods=['GET'])
 def get_fermata(fermata):
     string = "I passaggi per la fermata " + str(fermata) + " sono: <br>"
-    return string + gttorari.printout(gttorari.gttorari_stop(fermata))
-
-
-@app.route('/fermata/<int:fermata>/<int:line>', methods=['GET'])
-def get_fermata_line(fermata, line):
-    return gttorari.printnextpass(gttorari.gttorari_stop_line(fermata, line))
+    gtt = gttorari.printout(gttorari.gttorari_stop(fermata))
+    if gtt != "Errore: Fermata non trovata o sito non raggiungibile":
+        return string + gtt
+    else:
+        return gtt
 
 
