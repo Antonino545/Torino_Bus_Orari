0c28176b87b1fac023b0342ad6570fb1148fe49c
Antonino545 <80984937+Antonino545@users.noreply.github.com>
2023-10-18 17:00:56 +0200

    micro changes

--- static/index.js
+++ static/index.js
@@ -1,11 +1,12 @@
 function init() {
-    document.getElementById("stopForm").addEventListener("submit", function (e) {
-    e.preventDefault();
+    document.getElementById("stopForm").addEventListener("submit", listener);
+
+}
+//even listener
+function listener(e){
+        e.preventDefault();
     var stopValue = document.getElementById("stop").value;
     if (stopValue) {
         window.location.href = "fermata/" + stopValue;
     }
-});
-
 }
-
