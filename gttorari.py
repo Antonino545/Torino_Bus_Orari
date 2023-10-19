3ce1b54dd3b2dedd621511609573e892c2827d31
antonino <antonino.incorvaia@edu.unito.it>
2023-10-19 19:10:01 +0200

    errore

--- gttorari.py
+++ gttorari.py
@@ -4,11 +4,12 @@ import json
 import bs4
 import pytz
 import requests
+from requests import HTTPError
 
 
 def printout(data):
     var = ""
-    if data is requests.exceptions.HTTPError:
+    if data is None or data == [] or data == "" or data == HTTPError:
         return "Errore: Fermata non trovata o sito non raggiungibile"
     for bus_line, direction, pas, nextpass in data:
         var += "Linea: " + bus_line + " (" + direction + ")<br>"
@@ -24,8 +25,10 @@ def gttorari_url(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
-    except requests.exceptions.HTTPError as err:
+    except HTTPError as err:
+        print("Error: " + str(err))
         return err
+
     soup = bs4.BeautifulSoup(response.text, 'html.parser')
     data = []
     table = soup.find('tbody')
