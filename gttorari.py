db020e9270501ae92555e72ce42328979429f6a0
antonino <antonino.incorvaia@edu.unito.it>
2023-10-19 19:38:52 +0200

    errore fix

--- gttorari.py
+++ gttorari.py
@@ -1,16 +1,14 @@
-import csv
 import datetime
-import json
+
 import bs4
 import pytz
 import requests
-from requests import HTTPError
 
 
 def printout(data):
     var = ""
-    if data is HTTPError:
-        return "Errore: Fermata non trovata o sito non raggiungibile"
+    if data is "Errore: Fermata non trovata o sito non raggiungibile":
+        return data
     for bus_line, direction, pas, nextpass in data:
         var += "Linea: " + bus_line + " (" + direction + ")<br>"
         var += "Passaggi: " + pas + "<br>"
@@ -22,12 +20,20 @@ def printout(data):
 
 
 def gttorari_url(url):
+    global response
     try:
         response = requests.get(url)
         response.raise_for_status()
     except requests.exceptions.HTTPError as err:
         print(err)
-        return err
+        if response.status_code != 401:
+            return "Errore: Fermata non trovata o sito non raggiungibile"
+    if response.status_code == 401:
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            print(err)
     soup = bs4.BeautifulSoup(response.text, 'html.parser')
     data = []
     table = soup.find('tbody')
@@ -40,11 +46,17 @@ def gttorari_url(url):
             pas = cols[2].text.replace(
                 "Al momento non ci sono previsioni in tempo reale, clicca qui per visualizzare i passaggi programmati.",
                 "").strip()
+            print(pas)
             if pas != "":
+                print(pas)
                 if '*' in pas:
+                    print("*")
                     time_str = pas.split('*')[0]
                     hours, minutes = map(int, time_str.split(':'))
                 else:
+                    if '\xa0' in pas:
+                        print('\xa0')
+                        pas = pas.replace('\xa0', '')
                     time_str = pas.split(' ')[0]
                     hours, minutes = map(int, time_str.split(':'))
 
